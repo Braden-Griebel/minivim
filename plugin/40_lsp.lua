89d7c501d6ab6081fae00dd07f2f68e070b29e6e
Braden Griebel <bgriebel@uw.edu>
2025-10-29 14:16:42 -0700

    Updating plugin settings

--- plugin/40_lsp.lua
+++ plugin/40_lsp.lua
@@ -5,6 +5,10 @@ local now_if_args = _G.Config.now_if_args
 -- Language servers ===========================================================
 
 now_if_args(function()
+	-- Add and setup Mason
+	add({ source = "mason-org/mason.nvim" })
+	require("mason").setup({})
+
 	add("neovim/nvim-lspconfig")
 
 	vim.lsp.enable({
--- plugin/50_plugins.lua
+++ plugin/50_plugins.lua
@@ -4,167 +4,151 @@ local now_if_args = _G.Config.now_if_args
 
 -- Tree-sitter
 now_if_args(function()
-	add({
-		source = "nvim-treesitter/nvim-treesitter",
-		-- Use `main` branch since `master` branch is frozen, yet still default
-		checkout = "main",
-		-- Update tree-sitter parser after plugin is updated
-		hooks = {
-			post_checkout = function()
-				vim.cmd("TSUpdate")
-			end,
-		},
-	})
-	add({
-		source = "nvim-treesitter/nvim-treesitter-textobjects",
-		-- Same logic as for 'nvim-treesitter'
-		checkout = "main",
-	})
+  add({
+    source = "nvim-treesitter/nvim-treesitter",
+    -- Use `main` branch since `master` branch is frozen, yet still default
+    checkout = "main",
+    -- Update tree-sitter parser after plugin is updated
+    hooks = {
+      post_checkout = function()
+        vim.cmd("TSUpdate")
+      end,
+    },
+  })
+  add({
+    source = "nvim-treesitter/nvim-treesitter-textobjects",
+    -- Same logic as for 'nvim-treesitter'
+    checkout = "main",
+  })
 
-	-- Define languages which will have parsers installed and auto enabled
-	local languages = {
-		-- These are already pre-installed with Neovim. Used as an example.
-		"asm",
-		"c",
-		"cmake",
-		"cpp",
-		"css",
-		"fish",
-		"fortran",
-		"git_config",
-		"git_rebase",
-		"gitattributes",
-		"gitcommit",
-		"gitignore",
-		"gleam",
-		"go",
-		"haskell",
-		"html",
-		"ini",
-		"java",
-		"javadoc",
-		"javascript",
-		"json",
-		"julia",
-		"just",
-		"kdl",
-		"latex",
-		"lua",
-		"markdown",
-		"meson",
-		"nix",
-		"ocaml",
-		"pip_requirements",
-		"python",
-		"r",
-		"rust",
-		"sql",
-		"sway",
-		"typescript",
-		"typst",
-		"verilog",
-		"vimdoc",
-		"xml",
-		"yaml",
-		"zig",
-	}
-	local isnt_installed = function(lang)
-		return #vim.api.nvim_get_runtime_file("parser/" .. lang .. ".*", false) == 0
-	end
-	local to_install = vim.tbl_filter(isnt_installed, languages)
-	if #to_install > 0 then
-		require("nvim-treesitter").install(to_install)
-	end
+  -- Define languages which will have parsers installed and auto enabled
+  local languages = {
+    -- These are already pre-installed with Neovim. Used as an example.
+    "asm",
+    "c",
+    "cmake",
+    "cpp",
+    "css",
+    "fish",
+    "fortran",
+    "git_config",
+    "git_rebase",
+    "gitattributes",
+    "gitcommit",
+    "gitignore",
+    "gleam",
+    "go",
+    "haskell",
+    "html",
+    "ini",
+    "java",
+    "javadoc",
+    "javascript",
+    "json",
+    "julia",
+    "just",
+    "kdl",
+    "latex",
+    "lua",
+    "markdown",
+    "meson",
+    "nix",
+    "ocaml",
+    "pip_requirements",
+    "python",
+    "r",
+    "rust",
+    "sql",
+    "sway",
+    "typescript",
+    "typst",
+    "verilog",
+    "vim",
+    "vimdoc",
+    "xml",
+    "yaml",
+    "zig",
+  }
+  local isnt_installed = function(lang)
+    return #vim.api.nvim_get_runtime_file("parser/" .. lang .. ".*", false) == 0
+  end
+  local to_install = vim.tbl_filter(isnt_installed, languages)
+  if #to_install > 0 then
+    require("nvim-treesitter").install(to_install)
+  end
 
-	-- Enable tree-sitter after opening a file for a target language
-	local filetypes = {}
-	for _, lang in ipairs(languages) do
-		for _, ft in ipairs(vim.treesitter.language.get_filetypes(lang)) do
-			table.insert(filetypes, ft)
-		end
-	end
-	local ts_start = function(ev)
-		vim.treesitter.start(ev.buf)
-	end
-	_G.Config.new_autocmd("FileType", filetypes, ts_start, "Start tree-sitter")
+  -- Enable tree-sitter after opening a file for a target language
+  local filetypes = {}
+  for _, lang in ipairs(languages) do
+    for _, ft in ipairs(vim.treesitter.language.get_filetypes(lang)) do
+      table.insert(filetypes, ft)
+    end
+  end
+  local ts_start = function(ev)
+    vim.treesitter.start(ev.buf)
+  end
+  _G.Config.new_autocmd("FileType", filetypes, ts_start, "Start tree-sitter")
 end)
 
 -- Formatting =================================================================
 
 later(function()
-	add("stevearc/conform.nvim")
+  add("stevearc/conform.nvim")
 
-	require("conform").setup({
-		notify_on_error = false,
-		format_on_save = function(bufnr)
-			-- Disable "format_on_save lsp_fallback" for languages that don't
-			-- have a well standardized coding style. You can add additional
-			-- languages here or re-enable it for the disabled ones.
-			local disable_filetypes = {} -- { c = true, cpp = true }
-			if disable_filetypes[vim.bo[bufnr].filetype] then
-				return nil
-			else
-				return {
-					timeout_ms = 500,
-					lsp_format = "fallback",
-				}
-			end
-		end,
-		formatters_by_ft = {
-			lua = { "stylua" },
-			cpp = { "clang-format" },
-			bash = { "shfmt" },
-			markdown = { "mdformat" },
-			typst = { "typstyle" },
-			nix = { "alejandra" },
-		},
-		formatters = {
-			typstyle = {
-				command = "typstyle",
-				stdin = true,
-				args = { "--wrap-text" },
-			},
-		},
-	})
+  require("conform").setup({
+    notify_on_error = false,
+    format_on_save = function(bufnr)
+      local disable_filetypes = {} -- { c = true, cpp = true }
+      if disable_filetypes[vim.bo[bufnr].filetype] then
+        return nil
+      else
+        return {
+          timeout_ms = 500,
+          lsp_format = "fallback",
+        }
+      end
+    end,
+    formatters_by_ft = {
+      lua = { "stylua" },
+      cpp = { "clang-format" },
+      bash = { "shfmt" },
+      markdown = { "mdformat" },
+      typst = { "typstyle" },
+      nix = { "alejandra" },
+    },
+    formatters = {
+      typstyle = {
+        command = "typstyle",
+        stdin = true,
+        args = { "--wrap-text" },
+      },
+    },
+  })
 end)
+
+-- Linting ====================================================================
+later(
+  function()
+    add("mfussenegger/nvim-lint")
+
+    require("lint").linters_by_ft = {
+      markdown = { "markdownlint-cli2" },
+      bash = { "shellcheck" },
+      python = { "mypy" },
+      javascript = { "oxlint", "eslint" },
+      typscript = { "eslint" },
+    }
+
+    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+      callback = function()
+        require("lint").try_lint()
+      end,
+    })
+  end
+)
 
 -- Snippets ===================================================================
 
--- Although 'mini.snippets' provides functionality to manage snippet files, it
--- deliberately doesn't come with those.
---
--- The 'rafamadriz/friendly-snippets' is currently the largest collection of
--- snippet files. They are organized in 'snippets/' directory (mostly) per language.
--- 'mini.snippets' is designed to work with it as seamlessly as possible.
--- See `:h MiniSnippets.gen_loader.from_lang()`.
 later(function()
-	add("rafamadriz/friendly-snippets")
+  add("rafamadriz/friendly-snippets")
 end)
-
--- Honorable mentions =========================================================
-
--- 'mason-org/mason.nvim' (a.k.a. "Mason") is a great tool (package manager) for
--- installing external language servers, formatters, and linters. It provides
--- a unified interface for installing, updating, and deleting such programs.
---
--- The caveat is that these programs will be set up to be mostly used inside Neovim.
--- If you need them to work elsewhere, consider using other package managers.
---
--- You can use it like so:
--- later(function()
---   add('mason-org/mason.nvim')
---   require('mason').setup()
--- end)
-
--- Beautiful, usable, well maintained color schemes outside of 'mini.nvim' and
--- have full support of its highlight groups. Use if you don't like 'miniwinter'
--- enabled in 'plugin/30_mini.lua' or other suggested 'mini.hues' based ones.
--- MiniDeps.now(function()
---   -- Install only those that you need
---   add('sainnhe/everforest')
---   add('Shatur/neovim-ayu')
---   add('ellisonleao/gruvbox.nvim')
---
---   -- Enable only one
---   vim.cmd('color everforest')
--- end)
