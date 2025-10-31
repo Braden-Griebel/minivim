153a9e002f736d5cb2e4f68a06a6dff94bc81ffe
Braden Griebel <bgriebel@uw.edu>
2025-10-31 10:05:12 -0700

    Setup Tinymist pin keymaps
    Added keymaps for tinymist typst LSP for pinning and unpinning a "Main"
    file.

---
+++ after/lsp/tinymist.lua
@@ -0,0 +1,29 @@
+return {
+	on_attach = function(client, bufnr)
+		vim.keymap.set("n", "<localleader>tp", function()
+			client:exec_cmd({
+
+				title = "pin",
+
+				command = "tinymist.pinMain",
+
+				arguments = { vim.api.nvim_buf_get_name(0) },
+			}, { bufnr = bufnr })
+		end, { desc = "[T]inymist [P]in", noremap = true })
+
+		vim.keymap.set("n", "<localleader>tu", function()
+			client:exec_cmd({
+
+				title = "unpin",
+
+				command = "tinymist.pinMain",
+
+				arguments = { vim.v.null },
+			}, { bufnr = bufnr })
+		end, { desc = "[T]inymist [U]npin", noremap = true })
+		-- Add clue to mini clue
+		require("mini.clue").setup({ clues = {
+			{ mode = "n", keys = "<localleader>t", desc = "Tinymist" },
+		} })
+	end,
+}
